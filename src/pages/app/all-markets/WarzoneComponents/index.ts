56beb9967b038c24812ed06142d4edccad8606d2
Elad <elad2550@gmail.com>
2026-01-29 11:09:02 +0200

    update

--- src/pages/app/all-markets/WarzoneComponents/index.ts
+++ src/pages/app/all-markets/WarzoneComponents/index.ts
@@ -1,42 +1,6 @@
 // =====================================================
-// FINOTAUR WAR ZONE - Module Exports
+// FINOTAUR WAR ZONE - Module Exports (Minimal)
+// Avoid re-exporting sub-components to prevent circular deps
 // =====================================================
 
-// Main landing page component
 export { default } from './Warzonelanding';
-export { default as Warzonelanding } from './Warzonelanding';
-
-// Sub-components
-export { ActiveSubscriberView } from './ActiveSubscriberView';
-
-// Visual components
-export {
-  ParticleBackground,
-  SparkleEffect,
-  GlowingDust,
-  GoldenDivider,
-  GlowingBadge,
-  DiscordIcon,
-  BellIcon,
-  CompassIcon,
-  BullImage,
-  AmbientGlow,
-  FireGlow,
-  FullPageLoader,
-} from './VisualComponents';
-
-// Landing components
-export {
-  CONFIG,
-  YEARLY_SAVINGS,
-  BillingToggle,
-  StatsBar,
-  FeatureIcons,
-  ReportCard,
-  CountdownDisplay,
-  TestReportCard,
-  CommunityCards,
-  formatReportDate,
-  formatReportTime,
-  type BillingInterval,
-} from './WarzonelandingComponents';
